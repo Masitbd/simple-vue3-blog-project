43f9950c4622d42f9e55c483c74ddb1de9774e02
sarker24 <sarker24@gmail.com>
2023-08-31 00:15:02 +0200

    Successfully api call

---
+++ src/components/PostCard.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+        post: Object
+    
+})
+</script>
+<template>
+    <div>
+        {{ post.name }}
+    </div>
+</template>
+<style lang="scss" scoped>
+
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+import axios from 'axios'
+import PostCard from '../components/PostCard.vue';
 
-</script>
-
+let posts = reactive([]);
+    let isLoading = ref(true)
+    onMounted(async () => {
+        try {
+               const {data} = await axios.get("https://basic-blog.teamrabbil.com/api/post-categories")
+                posts = data;
+                console.log(data);
+                isLoading.value = false;              
+        } catch (error) {
+            console.error("Error fetching===>",error)
+            
+        }
+      })
+  </script>
 <template>
-  <main>
-   <h1>this is home</h1>
-  </main>
+    <h1 class="text-3xl font-bold" v-if="isLoading">Loading.....</h1>
+    <div class="grid grid-cols-3 gap-4 " v-else>
+        <PostCard
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+               />   
+</div>
 </template>
+
